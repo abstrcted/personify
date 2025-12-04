d7f467abef70b260319790dabd5a2d559a30cd72
abstrcted <primitivobambao@gmail.com>
2025-12-04 15:09:00 -0800

    removed one time js scripts

--- scripts/extract_pdf_text.js
+++
@@ -1,8 +0,0 @@
-// PDF extractor removed.
-// This file was a temporary helper used to extract text from Phase III PDF.
-// The functionality has been removed to avoid bundling PDF parsing code.
-// If you need to run PDF extraction locally, restore a secure helper and
-// install a trusted PDF parsing dependency. Do NOT commit secrets or
-// large extracted text files to the repository.
-
-console.log('extract_pdf_text.js: removed - no-op');
